49bb3d3e4c0e5ed8e84aff633f3da956425cf50c
aftabahmed1 <49833172+aftabahmed1@users.noreply.github.com>
2025-03-23 21:59:46 +0100

    test: add fixture files

---
+++ fixtures/testData.ts
@@ -0,0 +1,20 @@
+export const validLogin = {
+  username: 'standard_user',
+  password: 'secret_sauce'
+};
+
+export const invalidLogin = {
+  username: 'locked_out_user',
+  password: 'secret_sauce'
+};
+
+export const validOrderDetails = {
+  firstName: 'John',
+  lastName: 'Doe',
+  postalCode: '12345'
+}
+
+export const validAPILogin = {
+  email: 'eve.holt@reqres.in',
+  password: 'cityslicka'
+}
